b489e625031e41741484fe8cfa544953acdb1c9d
rafael-feliciano <rafael.cefetpg@gmail.com>
2021-04-13 15:47:59 -0300

    feat: create table user_client

--- app/models/user_client_model.py
+++ app/models/user_client_model.py
@@ -5,6 +5,17 @@ from . import db
 class UserClient(db.Model):
     __tablename__ = "user_client"
 
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+    email = db.Column(db.String(25), nullable=False, unique=True)
+    password_hash = db.Column(db.String(25), nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
+    adress = db.Column(db.String(25), nullable=False)
+    city = db.Column(db.String(15), nullable=False)
+    state = db.Column(db.String(15), nullable=False)
+
     @property
     def password(self):
         raise TypeError('A senha não pode ser acessada')
@@ -12,20 +23,10 @@ class UserClient(db.Model):
     @password.setter
     def password(self, new_password):
         new_password_hash = generate_password_hash(new_password)
-        self.password_hash = new_password_hash
+        self.password_hash = new_password_hashm
 
     def check_password(self, password_to_compare):
         return check_password_hash(self.password_hash, password_to_compare)
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(25), nullable=False)
-    email = db.Column(db.String(25), nullable=False, unique=True)
-    password = db.Column(db.String(25), nullable=False)
-    phone = db.Column(db.String(15), nullable=False)
-    adress = db.Column(db.String(25), nullable=False)
-    city = db.Column(db.String(15), nullable=False)
-    state = db.Column(db.String(15), nullable=False)
-
 
 
 

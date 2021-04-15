81a6b6c68cdd8fb74a7081bfdb130b04a5427a70
Felipe Souza <felipesouzadsgn@gmail.com>
2021-04-15 09:15:48 -0300

    refactor: service_model e view/__init__.py

--- app/models/service_model.py
+++ app/models/service_model.py
@@ -7,7 +7,7 @@ class ServiceModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     service_price = db.Column(db.Integer, nullable=False)
     service_description = db.Column(db.Text, nullable=False)
-    date_time =db.Column(db.String(25), nullable=False)
+    date_time = db.Column(db.String(25), nullable=False)
     link = db.Column(db.String(255))
     client_name = db.Column(db.String(55), nullable=False)
     client_email = db.Column(db.String(55), nullable=False, unique=True)

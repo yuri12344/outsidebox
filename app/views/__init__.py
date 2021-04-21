e6b4eab9bb5d1b2e282295f2ff62f419761a4c38
Yuri Caetano <yuuri.caetano@gmail.com>
2021-04-21 12:15:10 -0300

    delete migration

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -7,7 +7,5 @@ def init_app(app: Flask):
 
     # from app.views.service_view import bp_service
     # app.register_blueprint(bp_service)
-    #
     # from app.views.feedback_view import bp_feedback
     # app.register_blueprint(bp_feedback)
-    #

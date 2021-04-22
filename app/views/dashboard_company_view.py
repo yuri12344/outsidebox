028821c2eaae79f63476df1585b68917e6f8d76f
Yuri Caetano <yuuri.caetano@gmail.com>
2021-04-22 16:51:55 -0300

    feat: finish dashboard company and add base url to dotenv

--- app/views/dashboard_company_view.py
+++ app/views/dashboard_company_view.py
@@ -1,30 +1,40 @@
 from flask.globals import current_app
 from app.views.login_view import token_required
 from flask import Blueprint, request, jsonify
+import os
 
 dashboard_company = Blueprint(
     'dashboard_company', __name__, url_prefix='/company_dashboard/')
 
+base_url = os.getenv('BASE_URL')
+
 
 @dashboard_company.route('/<id_company>', methods=['GET'])
 @token_required
-def all_companys():
+def all_companys(id_company=0):
     user_logged = current_app.secret_key[2]['user']
     try:
         user_logged.get(user_logged['description'])
-        print(user_logged)
+        id_company_loged = user_logged['id']
         json_to_return = {
-            "company_name": "Mecanica do Pedro",
-            "clients_list": ["Yuri Caetano", "Felipe"],
-            "services_catalog": [{"service_name": "Limpeza de bico", "price": 50, "id_service": 1}],
-            "link_create_service_catalog": "/services_catalog/create/<id_company>",
-            "link_create_service_specific": "/services_specific/create/<id_company>",
-            "company_profile": "/company/<id_company>"
+            "your_id_company": user_logged['id'],
+            "company_name": user_logged['nome'],
+            "clients_list": [],
+            "services_catalog": user_logged['service_catalog_list'],
+            "link_create_service_catalog": f"{base_url}/services_catalog/create/{id_company_loged}",
+            "link_create_service_specific": f"{base_url}/services_specific/create/{id_company_loged}",
+            "company_profile": f"{base_url}/company/{id_company_loged}"
         }
+        if int(id_company) != int(user_logged['id']):
+            return jsonify(
+                {
+                    "error": "You cant see the others companys dashboards, returning your data only",
+                    "your_data": json_to_return
+                }
+            )
+        return jsonify(json_to_return)
     except KeyError:
         return jsonify({'message': 'This route is only for companys'})
-
-    return {"message": "You need to pass a id_company in URL ex: /company_dashboard/<id_company>/"}
 
 
 @dashboard_company.route('/', methods=['GET'])

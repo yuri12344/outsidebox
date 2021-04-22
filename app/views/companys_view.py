24eaca586f1a4431d684fcd470d57b8eb28e3173
rafael-feliciano <rafael.cefetpg@gmail.com>
2021-04-22 18:18:25 -0300

    return and routes companys_view

--- app/views/companys_view.py
+++ app/views/companys_view.py
@@ -1,15 +1,44 @@
 from flask import Blueprint, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from app.models.signup_company_model import CompanyModel
+import os
+base_url = os.getenv('BASE_URL')
 
-bp_companys = Blueprint(
-    'bp_companys', __name__)
-
-
-@bp_companys.route('/gti', methods=['GET'])
-def list_companys():
-
-    return {"message": "LIST ALL COMPANYS, but you can also give a id_company ex: companys/<id_company>"}
+bp_companys = Blueprint('bp_companys', __name__)
 
 
-@bp_companys.route('/teste/<int:id_company>', methods=['GET'])
+@bp_companys.route("/companys/")
+def companys():
+    companys = CompanyModel.query.all()
+    json_return = {}
+    conter = 1
+
+    for company in companys:
+        profile_url = base_url + "/company/" + str(company.id)
+        conter_str = str(conter)
+        concat = "company_" + conter_str
+        json_return[concat] = {"name": company.nome, "company_profile": profile_url}
+        conter += 1
+
+    print(companys)
+    return {"message": json_return}
+
+
+@bp_companys.route('/companys/<int:id_company>', methods=['GET'])
 def specific_company(id_company):
-    return jsonify({'message': f'This is all information about id_company {id_company}'})
+
+    # name = CompanyModel.query.filter_by(id=id_company).first_or_404(description='teste')
+    company = CompanyModel.query.get(id_company)
+
+    print(company.service_catalog_list)
+    return jsonify({"company_name": company.nome,
+                    "email": company.email,
+                    "phone": company.phone,
+                    "address": company.address,
+                    "city": company.city,
+                    "state": company.state,
+                    "cpf/cnpj": company.cpf_cnpj,
+                    "description": company.description,
+                    "schedule": company.schedule,
+                    "service_catolog": []
+                    })

cfa4dcc217898a20cd3509ff435f85b00cf7899f
Azuos Eplief <felipesouzadsgn@gmail.com>
2021-04-22 16:28:59 -0300

    update code service_catalog_view

--- app/views/service_request_catalog_model.py
+++ app/views/service_request_catalog_model.py
@@ -1,24 +1,42 @@
 from app.views.login_view import token_required
 from flask import Blueprint, request, jsonify
-
+from app.models.service_request_catalog_model import ServiceRequestCatalogModel
+from app.models.service_catalog_model import ServiceCatalogModel
+from app.models.signup_client_model import ClientModel
 bp_catalog_service_request = Blueprint(
-    'bp_catalog_service_request', __name__, url_prefix='/catalog_service_request')
+    'bp_catalog_service_request', __name__, url_prefix='/catalog_service_request/')
 
 
+@token_required
 @bp_catalog_service_request.route('/<id_company>', methods=['POST'])
-def service_catalog(id_company=0):
+def service_request_catalog(id_company=0):
     session = current_app.db.session
-    data = request.get_json()
 
-    session.add(service)
+    catalog_service_request = ServiceRequestCatalogModel(
+        client=data["name_of_service"],
+        data_time=data["data_time"],
+        informations=data["informations"],
+        feedback_url=data["feedback_url"],
+        aproved=data["aproved"],
+        responsible=data["responsible"],
+        id_company=id_company,
+        id_client=id_client,
+        id_service_catalog=id_service_catalog
+
+    )
+
+    session.add(catalog_service_request)
     session.commit()
+
     return {
-        "status": "created sucess",
-        "id": service.id,
-        "name_of_service": service.name_of_service,
-        "price": service.price,
-        "service_description": service.service_description,
-        "id_company": service.id_company
+        "id_service": "ID from  service in catalog ex: 1",
+        "client_name": "Optional",
+        "id_client": "Optional, if client have account put id_client here",
+        "date/time": "2020-20-05",
+        "informations": "links or extra informations",
+        "feedback_url": "",
+        "aproved": "Bool, True or False, if client aproved the service, put True, if is a budget, put False",
+        "responsible": "Felipe"
     }
 
 

d721edf769f20cefa8b895ef0f5ab7f175069140
Yuri Caetano <yuuri.caetano@gmail.com>
2021-04-22 16:27:58 -0300

    feat: do company dashboard

--- app/views/dashboard_company_view.py
+++ app/views/dashboard_company_view.py
@@ -1,15 +1,32 @@
+from flask.globals import current_app
+from app.views.login_view import token_required
 from flask import Blueprint, request, jsonify
 
 dashboard_company = Blueprint(
     'dashboard_company', __name__, url_prefix='/company_dashboard/')
 
 
-@dashboard_company.route('/', methods=['GET'])
-def slash_route():
+@dashboard_company.route('/<id_company>', methods=['GET'])
+@token_required
+def all_companys():
+    user_logged = current_app.secret_key[2]['user']
+    try:
+        user_logged.get(user_logged['description'])
+        print(user_logged)
+        json_to_return = {
+            "company_name": "Mecanica do Pedro",
+            "clients_list": ["Yuri Caetano", "Felipe"],
+            "services_catalog": [{"service_name": "Limpeza de bico", "price": 50, "id_service": 1}],
+            "link_create_service_catalog": "/services_catalog/create/<id_company>",
+            "link_create_service_specific": "/services_specific/create/<id_company>",
+            "company_profile": "/company/<id_company>"
+        }
+    except KeyError:
+        return jsonify({'message': 'This route is only for companys'})
 
     return {"message": "You need to pass a id_company in URL ex: /company_dashboard/<id_company>/"}
 
 
-@dashboard_company.route('/<id_company>', methods=['GET'])
-def specific_company(id_company=0):
-    return jsonify({'message': f'Dashboard company to id_company {id_company}'})
+@dashboard_company.route('/', methods=['GET'])
+def slash_route():
+    return jsonify({'message': f'You need login and was company and pass id_company'})

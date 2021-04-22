0700e278bf266316fa75fbdc8bdd6918fea88281
Yuri Caetano <yuuri.caetano@gmail.com>
2021-04-21 21:59:29 -0300

    delete migration

--- app/views/get_services_view.py
+++ app/views/get_services_view.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from app.models.signup_company_model import CompanyModel
 
 bp_get_services = Blueprint(
     'bp_get_services', __name__, url_prefix='/get_services/')
@@ -6,13 +8,21 @@ bp_get_services = Blueprint(
 
 @bp_get_services.route('/<id_company>/<id_service>', methods=['GET'])
 def service_catalog(id_company=0, id_service=0):
-    data = request.get_json()
     if id_company == 0 or id_service == 0:
         return jsonify({'message': 'id_company cannot be blank, id_service cannot be blank, you need to pass a valid value'})
+
+    get_company = CompanyModel.query.filter_by(id=id_company).first()
+    get_company = get_company.__dict__
+    print(get_company)
 
     return {"ROTA": "ROTA GET SERVICES"}
 
 
-@bp_get_services.route('/', methods=['GET'])
+@bp_get_services.route('/', methods=['GET', 'POST'])
 def only_slash():
-    return jsonify({'message': 'you need use url => get_services/<id_company>/<id_service> '})
+    return jsonify({'message': 'Use this url: /get_services/<id_company>/<id_service> '})
+
+
+@bp_get_services.route('/<id_company>', methods=['GET', 'POST'])
+def only_slash_and_one_number(id_company=0):
+    return jsonify({'message': 'Use this url: /get_services/<id_company>/<id_service> '})

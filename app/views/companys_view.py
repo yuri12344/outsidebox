6f7d2ad0f04550b82074c669a46bf5c73b5d4f18
rafael-feliciano <rafael.cefetpg@gmail.com>
2021-04-22 15:00:28 -0300

    fix: route companys_view

--- app/views/companys_view.py
+++ app/views/companys_view.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, request, jsonify
 
 bp_companys = Blueprint(
-    'bp_companys', __name__, url_prefix='/companys/')
+    'bp_companys', __name__)
 
 
-@bp_companys.route('/', methods=['GET'])
+@bp_companys.route('/gti', methods=['GET'])
 def list_companys():
 
     return {"message": "LIST ALL COMPANYS, but you can also give a id_company ex: companys/<id_company>"}
 
 
-@bp_companys.route('/<id_company>', methods=['GET'])
-def specific_company(id_company=0):
+@bp_companys.route('/teste/<int:id_company>', methods=['GET'])
+def specific_company(id_company):
     return jsonify({'message': f'This is all information about id_company {id_company}'})

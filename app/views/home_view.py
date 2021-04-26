7b3e6fed015abab4ae0b7b05406582418c0e4be8
Yuri Caetano <yuuri.caetano@gmail.com>
2021-04-26 02:03:46 -0300

    new string

--- app/views/home_view.py
+++ app/views/home_view.py
@@ -1,10 +1,14 @@
 from flask import Blueprint
 from flask.globals import current_app
 from flask.json import jsonify
+from os import getenv
 
 bp_home = Blueprint('/home', __name__, url_prefix='/')
 
-string = "postgres://ng"
+
+old_string = getenv('DATABASE_URL')
+
+new_string = old_string[:8] + "ql" + old_string[8:]
 
 
 @bp_home.route('/', methods=['GET'])
